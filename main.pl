08c78ae72480bc1d7ebc5e16a038e1682086ca65
mfauzanalg <mfauzanalg@gmail.com>
2019-11-17 08:46:22 -0800

    benerin Map

--- main.pl
+++ main.pl
@@ -43,9 +43,11 @@ do(s) :- s.
 do(d) :- d.
 do(map) :- map.
 do(status) :- status.
+do(drop(X)) :- drop(X).
 
 narasi:- 
-	write(' Hello there! Welcome to the world of Tokemon!'),nl.
+	write(' Hello there! Welcome to the world of Tokemon!'),nl,
+	write(' Anda terkunci di dalam barrier, temukan kunci untuk keluar!'),nl.
 
 help :-
 	write(' Available commands:'), nl,
@@ -61,87 +63,7 @@ help :-
 	write('    load(Filename). -- load previously saved game').
 
 quit:- halt.
-
-map:-
-	\+playing(_),
-	write('this command can only be used after the game starts.'), nl,
-	write('use "start." to start the Tokemon Game!'), nl, !.
-map:-
-	tinggipeta(T),
-	lebarpeta(L),
-	X is 0,
-	Xmax is L+1,
-	Y is 0,
-	Ymax is T+1,
-	forall(between(Y, Ymax, J), (
-		forall(between(X, Xmax, I), (
-			printpeta(I,J)
-		)),
-		nl
-	)),
-	write(' Legends:'), nl,
-	write('    X = Pagar'), nl,
-	write('    P = Player'), nl,
-	write('    G = Gym'), nl,nl,
-	!.
-
-% move
-w :-
-	\+playing(_),
-	write('this command can only be used after the game starts.'), nl,
-	write('use "start." to start the Tokemon Game!'), nl, !.
-w :-
-	player(_,Y),
-	Y =:= 1,
-	write('mentok cuy (nanti diperbaiki kata2nya)'),nl,!.
-w :- 
-	retract(player(X,Y)),
-	Y > 1,
-	NewY is Y - 1,
-	asserta(player(X, NewY)), !.
-
-a :-
-	\+playing(_),
-	write('this command can only be used after the game starts.'), nl,
-	write('use "start." to start the Tokemon Game!'), nl, !.
-a :-
-	player(X,_),
-	X =:= 1,
-	write('mentok cuy (nanti diperbaiki kata2nya)'),nl,!.
-a :- 
-	retract(player(X,Y)),
-	NewX is X - 1,
-	asserta(player(NewX, Y)), !.
-
-s :-
-	\+playing(_),
-	write('this command can only be used after the game starts.'), nl,
-	write('use "start." to start the Tokemon Game!'), nl, !.
-s :-
-	player(_,Y),
-	tinggipeta(P),
-	Y =:= P,
-	write('mentok cuy (nanti diperbaiki kata2nya)'),nl,!.
-s :- 
-	retract(player(X,Y)),
-	NewY is Y + 1,
-	asserta(player(X, NewY)), !.
-
-d :-
-	\+playing(_),
-	write('this command can only be used after the game starts.'), nl,
-	write('use "start." to start the Tokemon Game!'), nl, !.
-d :-
-	player(X,_),
-	lebarpeta(Q),
-	X =:= Q,
-	write('mentok cuy (nanti diperbaiki kata2nya)'),nl,!.
-d :- 
-	retract(player(X,Y)),
-	NewX is X + 1,
-	asserta(player(NewX, Y)), !.
 	
-
 %status
 status :-
 	\+playing(_),
