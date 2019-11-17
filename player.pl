1ec0af10035e32bcacbf08b04fb7d180f28b5707
Muhammad Rizki Fonna <13516001@std.stei.itb.ac.id>
2019-11-17 14:00:06 +0700

    Drop sudah jalan

--- player.pl
+++ player.pl
@@ -1,5 +1,5 @@
-:- include('map.pl').
-:- include('main.pl').
+%:- include('map.pl').
+%:- include('main.pl').
 
 
 countElmt([],0).
@@ -19,62 +19,78 @@ delete_one(Elmt, [Head|Tail], [Head|Result]) :-
 
 
 /*---------- N, S, W, E ----------*/
-n :- retract(player(I,J)), 
-	X is I-1, 
-	asserta(player(X,J)),
-	% message_location.
+% n :- retract(player(I,J)), 
+% 	X is I-1, 
+% 	asserta(player(X,J)),
+% 	% message_location.
 
-s :- retract(player(I,J)), 
-	X is I+1, 
-	asserta(player(X,J)),
-	% message_location.
+% s :- retract(player(I,J)), 
+% 	X is I+1, 
+% 	asserta(player(X,J)),
+% 	% message_location.
 
-w :- retract(player(I,J)),
-	 X is J-1, 
-	asserta(player(I,X)),
-	% message_location.
+% w :- retract(player(I,J)),
+% 	 X is J-1, 
+% 	asserta(player(I,X)),
+% 	% message_location.
 
-e :- retract(player(I,J)),
-	 X is J+1, 
-	asserta(player(I,X)),
-	% message_location.	
+% e :- retract(player(I,J)),
+% 	 X is J+1, 
+% 	asserta(player(I,X)),
+% 	% message_location.	
 
 /*---------- TAKE ----------*/
-take(Name) :- supply(Name,I,J),
-			 player(I,J),
-			 player_inventory(L,Max), 
-			 countElmt(L,X),
-			 X <Max, 
-			 append(L,[Name],Result),
-			 retract(player_inventory(L,Max)),
-			 asserta(player_inventory(Result,Max)),
-			 retract(supply(Name,I,J)),
-			%  message_take_succeed(Name), !.
+% take(Name) :- supply(Name,I,J),
+% 			 player(I,J),
+% 			 player_inventory(L,Max), 
+% 			 countElmt(L,X),
+% 			 X <Max, 
+% 			 append(L,[Name],Result),
+% 			 retract(player_inventory(L,Max)),
+% 			 asserta(player_inventory(Result,Max)),
+% 			 retract(supply(Name,I,J)),
+% 			%  message_take_succeed(Name), !.
 
-take(Name) :- supply(Name,I,J),
-			player(I,J), 
-			player_inventory(L,Max), 
-			countElmt(L,X), 
-			X == Max,
-			% message_take_inventfull, !.
+% take(Name) :- supply(Name,I,J),
+% 			player(I,J), 
+% 			player_inventory(L,Max), 
+% 			countElmt(L,X), 
+% 			X == Max,
+% 			% message_take_inventfull, !.
 
-take(Name) :-
-			player_position(I,J),
-			\+supply(Name,I,J), 
-			% message_take_notfound(Name).
+% take(Name) :-
+% 			player_position(I,J),
+% 			\+supply(Name,I,J), 
+% 			% message_take_notfound(Name).
 
 /*---------- DROP ----------*/
-drop(Name) :- player_inventory(L,Max),
-			is_member(Name,L),
-			delete_one(Name,L,X), 
-			retract(player_inventory(L,Max)),
-			asserta(player_inventory(X,Max)),
-			player(I,J),
-			asserta(supply(Name,I,J)),
+% drop(Name) :- player_inventory(L,Max),
+% 			is_member(Name,L),
+% 			delete_one(Name,L,X), 
+% 			retract(player_inventory(L,Max)),
+% 			asserta(player_inventory(X,Max)),
+% 			player(I,J),
+% 			asserta(supply(Name,I,J)),
+% 			% message_drop_succeed(Name), !.
+
+% drop(Name) :- player_inventory(L,_),
+% 			\+is_member(Name,L), 
+% 			% message_drop_notfound(Name).
+ 
+drop(Name) :- %player_inventory(L,Max),
+			%is_member(Name,L),
+			%delete_one(Name,L,X), 
+			%retract(player_inventory(L,Max)),
+			%asserta(player_inventory(X,Max)),
+			milik(Name, 1), 
+			retract(milik(Name, 1)),
+			asserta(milik(Name, 0)).
+			%player(I,J).
+			%asserta(supply(Name,I,J)),
 			% message_drop_succeed(Name), !.
 
-drop(Name) :- player_inventory(L,_),
-			\+is_member(Name,L), 
+drop(Name) :- %player_inventory(L,_),
+			\+milik(Name, 0),
+			write('Gagal'),
+			nl.
 			% message_drop_notfound(Name).
- 
-
